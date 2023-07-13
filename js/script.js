5ffa3821ee5dd0f5740cc73b79b0b6a4adbe3ce9
devseag <example@gmail.com>
2023-07-13 16:35:05 -0300

    initial project

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* Zadanija na urok:
+
+1) Udalit' vse reklamnye bloki so stranicy (pravaja chast' sajta)
+
+2) Izmenit' zhanr fil'ma, pomenjat' "komedija" na "drama"
+
+3) Izmenit' zadnij fon postera s fil'mom na izobrazhenie "bg.jpg". Ono lezhit v papke img.
+Realizovat' tol'ko pri pomoshhi JS
+
+4) Spisok fil'mov na stranice sformirovat' na osnovanii dannyh iz jetogo JS fajla.
+Otsortirovat' ih po alfavitu 
+
+5) Dobavit' numeraciju vyvedennyh fil'mov */
+
+'use strict';
+
+const movieDB = {
+	movies: [
+		"Logan",
+		"Liga spravedlivosti",
+		"La-la ljend",
+		"Oderzhimost'",
+		"Skott Piligrim protiv..."
+	]
+};
+
+// const box = document.querySelector('.promo__adv');
+
+// console.log(box);
